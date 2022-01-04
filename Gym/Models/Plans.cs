a899f6bd66f9d872decb05afef1f9e3abe04e58a
JoniPrifti <joni.prifti@fshnstudent.info>
2022-01-04 01:54:59 +0100

    baseCode

---
+++ Gym/Models/Plans.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Gym.Models
+{
+    public class Plans
+    {
+        [Required]
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name = "Duration")]
+        public string Duration { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        [Display(Name = "Price")]
+        public decimal Price { get; set; }
+        [Required]
+        [Display(Name = "Sessions")]
+        public int Sessions { get; set; }
+
+    }
+}
---
+++ Gym/Models/Trainier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gym.Models
+{
+    public class Trainier
+    {
+        public int Id { get; set; }
+        public string Emri { get; set; }
+        public string Mbiemri { get; set; }
+
+    }
+}
