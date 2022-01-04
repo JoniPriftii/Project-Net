162b712a6d39064722ed1126a261aad47b122882
SebastianBraushi <96844590+SebastianBraushi@users.noreply.github.com>
2022-01-04 13:49:26 +0100

    Update Dietat.cs

--- Gym/Models/Dietat.cs
+++ Gym/Models/Dietat.cs
@@ -11,12 +11,10 @@ public class Dietat
 	public Dietat()
 	{
 		public string DietId { get; set; }
-	public string ClientName { get; set; }
-	public int Age
-	{
-		get; set: }
+	        public string ClientName { get; set; }
+	        public int Age {get; set: }
 		public int Weight { get; set; }
-	public int Height { get; set; }
+	        public int Height { get; set; }
 
 }
 
