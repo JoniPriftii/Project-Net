7f9b0b010a476e3265e4358a5fbe2ed53d8fd999
Julian Rojba <julisnrojba@gmail.com>
2022-01-06 20:47:43 +0100

    regullova modelin

--- Gym/Models/Products.cs
+++ Gym/Models/Products.cs
@@ -11,30 +11,20 @@ public class Products
 
 
 {
+    [Display(Name="Id")]
     public int ID { get; set; }
+
+    [Display(Name = "Name")]
     public string Name { get; set; }
+
+    [Display(Name = "Category")]
     public string Category { get; set; }
+
+    [Display(Name = "Price")]
     public decimal Price { get; set; }
+
+    [Display(Name = "ImageName")]
     public string ImageName { get; set;}
-}
-}
-
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-
-namespace Products.Models;
-{ 
-public class Products
-
-
-{
-    public int ID { get; set; }
-    public string Name { get; set; }
-    public string Category { get; set; }
-    public decimal Price { get; set; }
 }
 }
 
