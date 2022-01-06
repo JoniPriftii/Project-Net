8b2f78ccd4f159a26ea795339720ec0f9fe24053
Ksina <ksina.gjokaj@gmail.com>
2022-01-06 09:37:12 +0100

    ndryshova property

--- Gym/Models/Trainiers.cs
+++ Gym/Models/Trainiers.cs
@@ -8,8 +8,10 @@ namespace Gym.Models
     public class Trainier
     {
         public int Id { get; set; }
-        public string Emri { get; set; }
-        public string Mbiemri { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Experience { get; set; }
+        
 
     }
 }
