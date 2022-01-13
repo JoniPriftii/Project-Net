ae3615269cbbfc8283b809e8f096296bb37c2a67
Sebastian <sbraushi24@gmail.com>
2022-01-13 23:09:54 +0100

    Required Properties

--- Gym/Models/Diets.cs
+++ Gym/Models/Diets.cs
@@ -7,23 +7,23 @@ namespace Gym.Models
 {
     public class Diets
     {
-        [Display(Name = "DietId")]
         public string DietId { get; set; }
        
         [Required(ErrorMessage = "Name is required")]
         [Display(Name = "ClientName")]
         public string ClientName { get; set; }
 
-        [Required(ErrorMessage = "Age is required")]
-        [Display(Name = "Age")]
-        public int Age { get; set; }
-
         [Required(ErrorMessage = "Weight is required")]
-        [Display(Name = "Weight")] 
+        [Display(Name = "Suggested Weight")] 
         public int Weight { get; set; }
 
         [Required(ErrorMessage = "Height is required")]
-        [Display(Name = "Height")]
+        [Display(Name = "Suggested Height")]
         public int Height { get; set; }
+
+        public string ImageName { get; set; }
+
+        [StringLength(500, ErrorMessage = "String must not be longer than 500" MinLength = "200")]
+        public string Description { get; set; }
     }
 }
